a57769fe5a8c315f4f8f7083d45f7730b4053083
Rayiku Nashira <rayikume@gmail.com>
2024-05-23 03:53:50 +0300

    Implementing ChatGPT summarizer api

--- src/components/Script.jsx
+++ src/components/Script.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Script = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   const handleSubmit = async (e) => {
-    alert("Submitted");
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
   };
 
   return (
